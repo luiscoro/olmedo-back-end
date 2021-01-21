6bb43e840b267f1c80130e1f7bbbc90d280e3843
PPAULCuentaGitHub <cuentamacppaul@gmail.com>
2021-01-20 20:18:00 -0500

    Commit JUNIT

---
+++ olmedorest/src/test/java/net/t6/olmedorest/FirstMockitoTest.java
@@ -0,0 +1,15 @@
+package net.t6.olmedorest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstMockitoTest {
+
+	@Test
+	void test() {
+		
+		assertTrue(true);
+	}
+
+}
